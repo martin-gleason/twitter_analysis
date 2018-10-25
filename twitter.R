32fac5073f03084ed9f15ee92b0d3dfa788f0c9f
marty-gleason <officergleason@gmail.com>
2018-10-25 16:59:18 -0500

    early attempt to get tweets, including two hashtags and 5 aaccounts

--- twitter.R
+++ twitter.R
@@ -3,3 +3,35 @@ library(tidyverse)
 library(twitteR)
 
 source("keys_and_tokens.R")
+
+setup_twitter_oauth(consumer_api, secret_api_key, access_token, Access_token_secret)
+
+#user IDs for intital twitter accounts
+cpd <-  getUser("@chicago_police")
+fbi <- getUser("@fbi")
+cpd_memorial <- getUser("@cpdmemorial")
+police_one <- getUser("@policeone")
+chicagos_finest <- getUser("@chicagosFines19")
+blue_lives <- getUser("@bluelivesmtr")
+
+#number string can be used to find users. its the twitter id.
+milwaukee_pd <- getUser("16806352")
+
+
+#Vectors of userIDs from Friends
+cpd_friends <- cpd$getFriendIDs()
+
+#Hash tag search. Beware the sewer
+tweets_blue_lives <- searchTwitter("#bluelivesmatter")
+back_blue <- searchTwitter("#BackTheBlue")
+
+
+#turns list into DF./ Just easier for me to read.
+tweets_Df <- tweets %>% twListToDF()
+
+#Get followers
+cpd_followers <- cpd$getFollowerIDs()
+blue_lives_followers <- blue_lives$getFollowerIDs()
+
+#get friendships
+friendships(c(blue_lives))
